8008746cb897290da573ce9dab90dd2073c1097f
Xalep4ik <xalep4ik@gmail.com>
2022-11-15 12:35:32 +0200

    python

---
+++ first_try.py
@@ -0,0 +1,24 @@
+amount = 10
+cookieprise = 8
+candyprice = 7
+
+rest = amount - cookieprise
+
+if rest >= candyprice:
+    print("I have enough money")
+else:
+    print("I haven't enough money ")
+
+
+amount -= cookieprise
+print("Amount is ", amount)
+
+if amount >= cookieprise:
+    print("I nave money")
+else:
+    print("I haven't money")
+
+type_x = type(amount)
+print(type_x)
+
+print(isinstance(amount, str))
